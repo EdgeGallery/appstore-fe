0f513b62def67418134a0d4a92959b3e403c73ee
chengrundong <upmhappy@163.com>
2020-12-22 16:03:12 +0800

    修改点：修改编辑失败的问题

--- src/pages/appPromote/Index.vue
+++ src/pages/appPromote/Index.vue
@@ -341,7 +341,7 @@ export default {
               }
             })
           } else {
-            myAppStore.modifyAppStoreApi(fd, '4c49a6f3-863b-5deb-bfc4-12ecd03502d8').then(res => {
+            myAppStore.modifyAppStoreApi(fd, this.form.appStoreId).then(res => {
               this.$message.success(this.$t('promptMessage.editAppStoreSuccess'))
               this.getAppPackageData()
               this.dialogVisible = false
