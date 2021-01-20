71516348bf22b476e210032a472fdfd230f68b63
zhaolongfei_123 <951084082@qq.com>
2021-01-20 15:35:32 +0800

    修改问题，#I2DW45 【appstore】消息中心中修改atp测试报告样式

--- src/pages/msgCenter/ATPReport.vue
+++ src/pages/msgCenter/ATPReport.vue
@@ -15,10 +15,20 @@
   -->
 
 <template>
-  <div class="report padding56">
-    <div class="report-content padding20">
-      <div class="report-tap">
-        <h3> {{ $t('report.testReport') }}</h3>
+  <div class="report padding20">
+    <div class="report-content">
+      <div class="logo">
+        <img
+          src="../../assets/images/logo.png"
+          alt=""
+        >
+        <div class="report-tap">
+          <h3> {{ $t('report.testReport') }}</h3>
+          <img
+            :src="resulticon"
+            alt=""
+          >
+        </div>
       </div>
       <div class="title">
         {{ $t('atp.baseInfo') }}
@@ -77,45 +87,64 @@
         <div class="title">
           {{ $t('report.reportanalysis') }}
         </div>
-        <div>
+        <div class="report_analysis">
           <el-row :gutter="20">
             <el-col
               :span="12"
-              class="chartPie"
             >
-              <ve-pie :data="chartData" />
+              <div class="left">
+                <div
+                  class="sumchart"
+                  id="leftchart"
+                />
+                <el-table :data="chartData">
+                  <el-table-column
+                    prop="name"
+                    :label="$t('report.model')"
+                  >
+                    <template slot-scope="scope">
+                      <span
+                        v-if="scope.row.name==='社区标准'"
+                        class="modelone"
+                      />
+                      <span
+                        v-else-if="scope.row.name==='移动企标'"
+                        class="modeltwo"
+                      />
+                      <span
+                        v-else-if="scope.row.name==='联通企标'"
+                        class="modelthree"
+                      />
+                      <span
+                        v-else-if="scope.row.name==='电信企标'"
+                        class="modelfour"
+                      />
+                      <span
+                        v-else-if="scope.row.name==='自定义标准'"
+                        class="modelfive"
+                      />
+                      <span>{{ scope.row.name }}</span>
+                    </template>
+                  </el-table-column>
+                  <el-table-column
+                    prop="value"
+                    label="数量"
+                    width="100"
+                  />
+                </el-table>
+              </div>
             </el-col>
             <el-col
-              :span="10"
-              class="chartLine"
-              style="margin-top:40px;"
+              :span="12"
             >
-              <el-table
-                :data="modelData"
-                border
-                stripe
-              >
-                <el-table-column
-                  prop="model"
-                  :label="$t('report.model')"
-                />
-                <el-table-column
-                  prop="success"
-                  :label="$t('atp.success')"
-                />
-                <el-table-column
-                  prop="fail"
-                  :label="$t('atp.failed')"
-                />
-                <el-table-column
-                  prop="passRate"
-                  :label="$t('report.rate')"
-                />
-              </el-table>
+              <div
+                class="detailchart"
+                id="rightchart"
+              />
             </el-col>
           </el-row>
         </div>
-        <div>
+        <div class="report_detail">
           <div class="title">
             {{ $t('report.reportDetail') }}
           </div>
@@ -253,20 +282,21 @@ export default {
           passRate: 0
         }
       ],
+      successData: [],
+      failData: [],
+      passRateData: [],
       activeNames: ['securityTest'],
       securityTest: [],
       complianceTest: [],
       sandboxTest: [],
-      chartData: {
-        columns: ['status', 'case'],
-        rows: [
-          { status: '社区标准用例数', case: 0 },
-          { status: '移动企标用例数', case: 0 },
-          { status: '联通企标用例数', case: 0 },
-          { status: '电信企标用例数', case: 0 },
-          { status: '自定义标准用例数', case: 0 }
-        ]
-      }
+      chartData: [
+        { name: '社区标准', value: 0 },
+        { name: '移动企标', value: 0 },
+        { name: '联通企标', value: 0 },
+        { name: '电信企标', value: 0 },
+        { name: '自定义标准', value: 0 }
+      ],
+      resulticon: ''
     }
   },
   mounted () {
@@ -281,6 +311,11 @@ export default {
     getReport () {
       processApi(this.dataurl).then(res => {
         this.tableData.push(res.data)
+        if (this.tableData[0].status === 'success') {
+          this.resulticon = require('../../assets/images/chenggong.png')
+        } else {
+          this.resulticon = require('../../assets/images/shibai.png')
+        }
         this.tableData.forEach(item => {
           let newDateBegin = this.dateChange(item.createTime)
           item.createTime = newDateBegin
@@ -312,6 +347,7 @@ export default {
         }
         this.getModelData()
         this.getChartData()
+        this.getRightChart()
       })
     },
     getModelData () {
@@ -440,12 +476,145 @@ export default {
     getChartData () {
       // 通过率
       for (let index = 0; index < this.modelData.length; index++) {
-        let chartitem = this.chartData.rows[index]
+        let chartitem = this.chartData[index]
         const item = this.modelData[index]
-        chartitem.case = item.success + item.fail
-        item.passRate = (item.success / (item.success + item.fail) * 100).toFixed(2) + '%'
+        chartitem.value = item.success + item.fail
+        // item.passRate = (item.success / (item.success + item.fail) * 100).toFixed(2) + '%'
+        item.passRate = (item.success / (item.success + item.fail) * 100).toFixed(2)
       }
+      this.drawLeftLine()
     },
+    getRightChart () {
+      this.modelData.forEach((item) => {
+        this.successData.push(item.success)
+        this.failData.push(item.fail)
+        this.passRateData.push(item.passRate)
+      })
+      this.drawRightLine()
+    },
+    drawLeftLine () {
+      let Chart = this.$echarts.init(document.getElementById('leftchart'))
+      let colors = ['#89a6e6', '#a8d89b', '#9ed0c9', '#deba69', '#baa3d4']
+      let option = {
+        color: colors,
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        series: [
+          {
+            name: '认证模板',
+            type: 'pie',
+            radius: ['20%', '40%'],
+            avoidLabelOverlap: false,
+            labelLine: {
+              show: false
+            },
+            data: this.chartData
+          }
+        ]
+      }
+      Chart.setOption(option)
+    },
+    drawRightLine () {
+      let passChart = this.$echarts.init(document.getElementById('rightchart'))
+      var colors = ['#89a6e6', '#f59898', '#675bba']
+      let option = {
+        color: colors,
+        tooltip: {
+          trigger: 'axis'
+        },
+        grid: {
+          right: '25%'
+        },
+        legend: {
+          left: 10,
+          data: ['成功数量', '失败数量', '通过率']
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisTick: {
+              alignWithLabel: true
+            },
+            data: ['社区标准', '移动企标', '联通企标', '电信企标', '自定义标准']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            name: '成功数量',
+            min: 0,
+            max: 80,
+            position: 'right',
+            axisLine: {
+              lineStyle: {
+                color: colors[0]
+              }
+            },
+            axisLabel: {
+              formatter: '{value}'
+            }
+          },
+          {
+            type: 'value',
+            name: '失败数量',
+            min: 0,
+            max: 80,
+            position: 'right',
+            offset: 80,
+            axisLine: {
+              lineStyle: {
+                color: colors[1]
+              }
+            },
+            axisLabel: {
+              formatter: '{value}'
+            }
+          },
+          {
+            type: 'value',
+            name: '成功率',
+            min: 0,
+            max: 100,
+            position: 'left',
+            axisLine: {
+              lineStyle: {
+                color: colors[2]
+              }
+            },
+            axisLabel: {
+              formatter: '{value} %'
+            }
+          }
+        ],
+        series: [
+          {
+            name: '成功数量',
+            type: 'bar',
+            data: this.successData
+          },
+          {
+            name: '失败数量',
+            type: 'bar',
+            yAxisIndex: 1,
+            data: this.failData
+          },
+          {
+            name: '通过率',
+            type: 'line',
+            symbolSize: 15,
+            lineStyle: {
+              color: '#6c50f3'
+            },
+            yAxisIndex: 2,
+            data: this.passRateData
+          }
+        ]
+      }
+      passChart.setOption(option)
+    },
+
     dateChange (dateStr) {
       if (dateStr) {
         let date = new Date(Date.parse(dateStr))
@@ -492,22 +661,46 @@ export default {
 }
 </script>
 
-<style lang='less' scoped>
+<style lang='less'>
 .report {
   .report-content {
     background: white;
     background-image: url('../../assets/images/edgegallery.png');
-    .report-tap {
-      h3 {
-        text-align: center;
-        font-size: 25px;
-        color: #55565df2;
+     .logo {
+      display: flex;
+      justify-content: space-between;
+      height: 65px;
+      line-height: 65px;
+      padding-left: 25px;
+      img {
+        height: 65px;
       }
-      p {
-        color: #3399ff;
-        text-align: center;
+      .report-tap {
+        display: flex;
+        margin: auto;
+        line-height: 65px;
+        img{
+          margin-top: 5px;
+          width: 50px;
+          height: 50px;
+          line-height: 65px;
+        }
+        h3 {
+          text-align: center;
+          font-size: 25px;
+          color: #55565df2;
+        }
+        p {
+          color: #3399ff;
+          text-align: center;
+        }
+      }
+      .download{
+        text-align: right;
+        margin-right: 30px;
       }
     }
+
     .app-img {
       text-align: center;
       border: 1px solid #ddd;
@@ -522,6 +715,7 @@ export default {
       margin-left: 25px;
     }
     .title {
+      font-size: 20px;
       margin: 15px 0;
     }
     .title::before {
@@ -534,19 +728,84 @@ export default {
       top: 5px;
     }
     .report-chart {
-      margin: 25px 0;
-      .el-table thead {
-          color: #909399;
-          font-weight: 800;
-          th{
-            background: #f5f5f6;
+      .report_analysis{
+        .left{
+          display: flex;
+          .el-table{
+            margin-top: 45px;
+          }
+          .el-table::before{
+            background-color: #fff;
           }
         }
-          .el-collapse-item__header{
-            font-size: 16px;
+        .sumchart{
+          width: 60%;
+          height: 400px;
+        }
+        .modelone,.modeltwo,.modelthree,.modelfour,.modelfive{
+          width: 8px;
+            height: 8px;
+            display: inline-block;
+            margin-right: 10px;
+            border-radius: 50%;
+        }
+        .modelone{
+          background-color: #89a6e6;
+        }
+        .modeltwo{
+          background-color: #a8d89b;
+        }
+        .modelthree{
+          background-color: #9ed0c9;
+        }
+        .modelfour{
+          background-color: #deba69;
+        }
+        .modelfive{
+          background-color: #baa3d4;
+        }
+        .detailchart{
+          width: 100%;
+          height: 400px;
+        }
+      }
+      .report_detail{
+        .el-collapse-item__header{
+            font-size: 17px;
             background-color: #FAFAFA;
-            margin-left: 20px;
+            padding-left: 20px;
           }
+        // .el-collapse-item__header:before {
+        //   content: "";
+        //   display: inline-block;
+        //   border: 8px solid #fff;
+        //   border-left-color: rgb(101, 129, 221);
+        //   border-right: 0;
+        //   position: relative;
+        //   right: 10px;
+        //   animation: rotate 1s infinite;
+        // }
+        // .isActive{
+        //   animation: rotate 1s infinite;
+        // }
+        // @keyframes rotate {
+        //   from {
+        //       transform: rotate(0deg);
+        //   }
+        //   to {
+        //       transform: rotate(90deg);
+        //   }
+        // }
+      }
+    }
+    @media screen and (max-width: 1380) {
+      .sumchart{
+          width: 60%;
+          height: 300px;
+      }
+    }
+    .el-table{
+      font-size: 16px;
     }
   }
 }
