937590bd2b50436b3b6779360d4d7e183ac859df
13165725752@163.com <13165725752@163.com>
2021-03-17 22:25:09 +0800

    问题单修改

--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -138,8 +138,10 @@
       </h3>
       <p class="home_more">
         <el-link
+          v-for="(item,index) in scoreData"
+          :key="index"
+          @click="selectedCondition(item.type,item.index)"
           :underline="false"
-          @click="selectedCondition1('Score', 2)"
         >
           {{ $t('store.viewMore') }}
         </el-link>
@@ -342,7 +344,7 @@
 </template>
 
 <script>
-import { INDUSTRY, TYPES, AFFINITY } from '../../tools/constant.js'
+import { INDUSTRY, TYPES, AFFINITY, SORT_BY } from '../../tools/constant.js'
 import uploadPackage from '../home/UploadPackage.vue'
 // import homes from '../home/Index.vue'
 import { mapState } from 'vuex'
@@ -366,6 +368,11 @@ export default {
         require('../../assets/images/home_new_app5.png'),
         require('../../assets/images/home_new_app6.png')
       ],
+      scoreData: [
+        {
+          type: 'sortBy',
+          index: 2
+        }],
       recommendData: [
         {
           imgSrc: require('../../assets/images/home_recommend_pic1.jpg'),
@@ -445,6 +452,7 @@ export default {
       industry: INDUSTRY,
       types: TYPES,
       affinity: AFFINITY,
+      sortBy: SORT_BY,
       contactUrl: 'http://www.edgegallery.org',
       giteeUrl: 'https://gitee.com/edgegallery',
       developerUrl: '',
@@ -492,7 +500,7 @@ export default {
     },
     selectedCondition2 () {
       this.selectedConditions = []
-      let types = ['types', 'affinity', 'industry']
+      let types = ['types', 'affinity', 'industry', 'sortBy']
       if (this.selectedConditions) {
         types.forEach((item) => {
           this[item].forEach((condition) => {
