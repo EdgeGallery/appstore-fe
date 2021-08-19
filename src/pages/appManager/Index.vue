bcb38e1e7ecb2586c9fe2dc64bc570225a5f579e
13165725752@163.com <13165725752@163.com>
2021-08-19 14:33:27 +0800

    应用管理界面优化

--- src/pages/appManager/Index.vue
+++ src/pages/appManager/Index.vue
@@ -16,68 +16,72 @@
 
 <template>
   <div class="myApp padding56">
-    <div class="appManager-content">
-      <div class="appManagerTabs">
-        <el-tabs
-          type="border-card"
-          @tab-click="handleClick"
-        >
-          <el-tab-pane>
-            <span slot="label">
-              <img
-                :src="activeTabIndex==='0'?popularAbleIcon:popularEnableIcon"
-                class="img-icon"
-                alt=""
-              >
-              {{ $t('appManager.hotAppTitle') }}
-            </span>
-            <div
-              v-if="activeTabIndex==='0'"
-              class="appManagerTab"
-            >
-              <hotAppManager
-                v-model="activeTabIndex"
-              />
-            </div>
-          </el-tab-pane>
-          <el-tab-pane>
-            <span slot="label">
-              <img
-                :src="activeTabIndex==='1'?displayAbleIcon:displayEnableIcon"
-                class="img-icon"
-                alt=""
-              >
-              {{ $t('appManager.storeAppTitle') }}
-            </span>
-            <div
-              v-if="activeTabIndex==='1'"
-              class="appManagerTab"
-            >
-              <storeAppManager
-                v-model="activeTabIndex"
-              />
-            </div>
-          </el-tab-pane>
-          <el-tab-pane>
-            <span slot="label">
-              <img
-                :src="activeTabIndex==='2'?pushAbleIcon:pushEnableIcon"
-                class="img-icon"
-                alt=""
-              >
-              {{ $t('appManager.pushAppTitle') }}
-            </span>
-            <div
-              v-if="activeTabIndex==='2'"
-              class="appManagerTab"
-            >
-              <pushAppManager
-                v-model="activeTabIndex"
-              />
-            </div>
-          </el-tab-pane>
-        </el-tabs>
-      </div>
+    <div class="headerTitle">
+      <p class="subheaderTitle">
+        {{ $t('nav.appMgmt') }}
+      </p>
+      <div class="subBack" />
+    </div>
+    <ul class="list_top clear">
+      <li
+        @click="activeName='hot'"
+        :class="{'container_active':activeName==='hot','container_default':activeName==='store','container_default2':activeName==='push',}"
+      >
+        <span>
+          <em class="image_container" />{{ $t('appManager.hotAppTitle') }}
+        </span>
+      </li>
+      <li
+        @click="activeName='store'"
+        :class="{'vm_active':activeName==='store','vm_default':activeName==='hot','vm_default2':activeName==='push'}"
+      >
+        <span>
+          <link-right
+            v-if="activeName==='push'"
+            padding-right="5px"
+            margin="5px"
+            class="link-right"
+          />
+          <em class="image_vm" />{{ $t('appManager.storeAppTitle') }}
+        </span>
+      </li>
+      <li
+        @click="activeName='push'"
+        :class="{'push_active':activeName==='push','push_default':activeName!=='push','push_default2':activeName==='store'}"
+      >
+        <span>
+          <link-right
+            v-if="activeName==='hot'"
+            padding-right="5px"
+            margin="5px"
+            class="link-right"
+          />
+          <em class="image_container" />{{ $t('appManager.pushAppTitle') }}
+        </span>
+      </li>
+      <li
+        class="last_li"
+        :class="{'push_active':activeName==='push','last_default':activeName!=='push','last_default2':activeName==='push'}"
+      >
+        <span />
+      </li>
+    </ul>
+    <div
+      class="container_div"
+      :class="{'container_div_active':activeName!=='hot'}"
+    >
+      <hotAppManager
+        v-show="activeName==='hot'"
+        ref="hotAppManager"
+      />
+      <storeAppManager
+        v-show="activeName==='store'"
+        ref="storeAppManager"
+      />
+      <pushAppManager
+        v-show="activeName==='push'"
+        ref="pushAppManager"
+      />
     </div>
   </div>
 </template>
@@ -100,6 +104,7 @@ export default {
   },
   data () {
     return {
+      activeName: 'hot',
       activeTabIndex: '0',
       popularEnableIcon: popularEnableIcon,
       popularAbleIcon: popularAbleIcon,
@@ -124,39 +129,216 @@ export default {
 </script>
 <style lang='less'>
 .myApp {
-  .appManager-content {
-    background: white;
-    margin: 95px auto;
-    width: 1416px;
-    height: 800px;
-    border: none;
-    .appManagerTabs{
-      width: 1416px;
-      text-align: left;
-      border: none;
-      .el-tabs--border-card > .el-tabs__header {
-        background-color: #F0F2F5;
+  border: none;
+  .headerTitle{
+    margin-top: 59px;
+    margin-left: 126px;
+    .subheaderTitle {
+        font-size: 30px;
+        font-weight: bold;
+        color: #5d3da0;
+        font-family: HarmonyOS_Sans_SC_Bold;
       }
-      .img-icon{
-        width: 14px;
-        height: 14px;
-      }
-      .el-tabs__item.is-active{
-        background-color: #FEFEFE;
-        color: #7390E3;
-        border-top-right-radius: 50px;
-      }
-      .el-tabs__item{
-        font-size: 16px;
-        background-color: #E3E8F7;
-        color: #4B4B4B;
-        border-top-right-radius: 50px;
-      }
-      .appManagerTab{
-        width: 1416px;
-        height: 800px;
+    .subBack{
+      // margin-top: 51px;
+      background-color: rgba(158,123,205,.2);
+      width: 88px;
+      height: 7px;
+      border-radius: 16pt  16pt   16pt  16pt;
+    }
+
+  }
+  .list_top{
+    margin-top: 58px;
+    li{
+      float: left;
+      height: 50px;
+      line-height: 50px;
+      cursor: pointer;
+      background: #D4D1EC;
+      span{
+        display: block;
+        width: 100%;
+        height: 100%;
+        padding: 0 30px;
+        font-size: 20px;
+        color: #fff;
+        transition: all 0.5s;
       }
     }
+    .last_li{
+      cursor: default;
+    }
+    em{
+      display: inline-block;
+      width: 19px;
+      height: 22px;
+      position: relative;
+      top: 4px;
+      margin-right: 10px;
+    }
+    .image_container{
+      background: url('../../assets/images/popular_enable_icon.png');
+    }
+    .image_vm{
+      background: url('../../assets/images/display_enable_icon.png');
+    }
+    .container_active .image_container{
+      background: url('../../assets/images/popular_able_icon.png');
+    }
+    .vm_active .image_vm{
+      background: url('../../assets/images/display_able_icon.png');
+    }
+    .push_active .push_container{
+      background: url('../../assets/images/push_able_icon.png');
+    }
+    .push_container{
+      background: url('../../assets/images/push_enable_icon.png');
+    }
+    .container_active{
+      background: #d4d1ec;
+      border-radius: 16px 0 0 0;
+      transition: all 0.5s;
+      span{
+        background: #fff;
+        border-radius: 16px 16px 0 0;
+        color: #5e40c8;
+        transition: all 0.5s;
+      }
+    }
+    .container_default{
+      background: #fff;
+      border-radius: 16px 0 0 0;
+      transition: all 0.5s;
+      span{
+        background: #d4d1ec;
+        border-radius: 16px 0 16px 0;
+        transition: all 0.5s;
+      }
+    }
+    .container_default2{
+      background: #d4d1ec;
+      border-radius: 16px 0 0 0;
+    }
+    .container_push_default{
+      background: #fff;
+      border-radius: 0 0 0 0;
+      transition: all 0.5s;
+      span{
+        background: #d4d1ec;
+        border-radius: 0 0 0 0;
+        transition: all 0.5s;
+      }
+
+    }
+    .vm_active{
+      background: #d4d1ec;
+      transition: all 0.5s;
+      span{
+        background: #fff;
+        border-radius: 16px 16px 0 0;
+        color: #5e40c8;
+        transition: all 0.5s;
+      }
+    }
+    .vm_default{
+      background: #fff;
+      border-radius: 0 16px 0 0;
+      transition: all 0.5s;
+      span{
+        background: #d4d1ec;
+        border-radius: 0 0 0 16px;
+        transition: all 0.5s;
+      }
+    }
+    .vm_default2{
+      background: #fff;
+      span{
+        background: #d4d1ec;
+        border-radius: 0 0 16px 0;
+      }
+    }
+    .last_li.last_active{
+      background: #fff;
+      span{
+        background: #f5f6f8;
+        border-radius: 0 0 0 0;
+      }
+    }
+    .last_li.last_default{
+      background: #d4d1ec;
+      span{
+        background: linear-gradient(to bottom, #f5f4f8, #f1edf6);
+        border-radius: 0 0 0 16px;
+        transition: all 0.5s;
+      }
+    }
+    .last_li.last_default2{
+      background: #fff;
+      span{
+        background: linear-gradient(to bottom, #f5f4f8, #f1edf6);
+        border-radius: 0 0 0 16px;
+        transition: all 0.5s;
+      }
+    }
+    .push_active{
+      background: #d4d1ec;
+      border-radius: 0 16px 0 0;
+      transition: all 0.5s;
+      span{
+        background: #fff;
+        border-radius: 16px 16px 0 0;
+        color: #5e40c8;
+        transition: all 0.5s;
+      }
+    }
+    .push_default{
+      background: #f4f3f7;
+      // border-radius: 0 16px 0 0;
+      transition: all 0.5s;
+      span{
+        background: #d4d1ec;
+        border-radius: 0 16px 0 0px;
+        transition: all 0.5s;
+      }
+
+    }
+    .push_default2{
+      background: #fff;
+      border-radius: 0 16px 0 0;
+
+      span{
+        border-radius: 0 16px 0 16px;
+      }
+    }
+  }
+  .container_div{
+    background: #fff;
+    border-radius: 0 16px 16px 16px;
+    transition: all 0.5s;
+    box-shadow: 0 0 68px 5px rgba(94,24,200,0.06);
+  }
+  .container_div_active{
+    background: #d4d1ec;
+  }
+  .vm_div{
+    background: #d4d1ec;
+    border-radius: 0 16px 16px 16px;
+    transition: all 0.5s;
+  }
+  .view_image_dialog .el-dialog{
+    width: 40%;
+    min-width: 600px;
+  }
+  .link-left {
+    width: 3px;
+    height: 10px;
+    border-left: solid #B3B0CA 2px;
+  }
+  .link-right {
+    width: 3px;
+    height: 10px;
+    border-right: solid #B3B0CA 2px;
   }
 }
 
