2657d0e58d5e25c90f3192c2d0e5503f85b5d6f0
zhouyanbing <zhouyanbing.zhou@huawei.com>
2021-11-18 15:07:48 +0800

    命名调整

--- src/tools/comon.js
+++ src/tools/comon.js
@@ -18,7 +18,7 @@ import {
   PLATFORM_LIST,
   PROXY_PREFIX_CURRENTSERVER,
   PORT_CURRENTSERVER,
-  DOMAINNAME_CURRENTSERVER
+  DOMAIN_CURRENTSERVER
 } from './constant.js'
 
 let common = {
@@ -27,7 +27,7 @@ let common = {
 
     let _proxyPrefix = _platform ? _platform.proxyPrefix : PROXY_PREFIX_CURRENTSERVER
     let _port = _platform ? _platform.port : PORT_CURRENTSERVER
-    let _domainName = _platform ? _platform.port : DOMAINNAME_CURRENTSERVER
+    let _domain = _platform ? _platform.domain : DOMAIN_CURRENTSERVER
 
     let _originUrl = window.location.origin
     if (PROXY_PREFIX_CURRENTSERVER) {
@@ -37,7 +37,7 @@ let common = {
     if (_originUrl.indexOf(PORT_CURRENTSERVER) !== -1) {
       return _originUrl.replace(PORT_CURRENTSERVER, _port)
     } else {
-      return _originUrl.replace(DOMAINNAME_CURRENTSERVER, _domainName)
+      return _originUrl.replace(DOMAIN_CURRENTSERVER, _domain)
     }
   },
   setDivHeightFun: function (screenHeight, className, otherHeight) {
